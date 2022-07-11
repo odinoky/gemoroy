010dddae85a08bb33b6f2abaf9881a25f09e3377
FREDOM1979 <5baxoff@gmail.com>
2022-07-11 20:30:16 +0600

    пшапшаш7ееее

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.ownerNumber = ['77072763560','996755994412']
 global.ownerName = '*ВАНИЛЬКА*'
 global.packname = 'стикеры'
 global.author = '*от ВАНИЛЬКИ*'
-global.prefa = ['#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
+prefix = ['#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'sesi'
 
 //Message Nya Ubah Disini
