5f0bd15c07f9656e5689819f9e8961c438499495
odinoky <93214372+odinoky@users.noreply.github.com>
2023-06-17 12:05:53 +0600

    teku6

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -167,7 +167,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
 }
 handler.help = ['help']
 handler.tags = ['main']
-handler.command = ['menu', 'help', 'menú'] 
+handler.command = ['менюшкаа'] 
 handler.register = false
 
 handler.exp = 3
